cb9ca3576aa3aaa6f15c2f3e1b0ee7c7743be286
Garrick <ContactGarrick@gmail.com>
2021-01-01 22:00:02 -0800

    no message

--- gs-serving-web-content/initial/src/main/java/com/example/servingwebcontent/MovieListController.java
+++ gs-serving-web-content/initial/src/main/java/com/example/servingwebcontent/MovieListController.java
@@ -9,6 +9,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class MovieListController {
 
+	@GetMapping("/greeting")
+	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
+	}
+
 	@GetMapping("/navbar")
 	public String navbar() {
 		return "navbar";
