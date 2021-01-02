1d02e58e1e666216b4abd1a4d3ca2fd43f296169
Garrick <ContactGarrick@gmail.com>
2021-01-01 17:59:48 -0800

    added view files

---
+++ gs-serving-web-content/initial/src/main/java/com/example/servingwebcontent/MovieListController.java
@@ -0,0 +1,27 @@
+
+package com.example.servingwebcontent;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+
+	@GetMapping("/greeting")
+	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
+	}
+
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+
+	@GetMapping("/register")
+	public String login() {
+		return "register";
+	}
+}
