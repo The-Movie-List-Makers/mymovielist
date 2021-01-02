dd3bbdd73d909d8cb3a9e28c60a03d6a863da1d5
Garrick <ContactGarrick@gmail.com>
2021-01-01 21:36:29 -0800

    cleaned up directory

---
+++ root/src/main/java/com/example/servingwebcontent/LoginController.java
@@ -0,0 +1,16 @@
+package com.example.servingwebcontent;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+
+}
---
+++ root/src/main/java/com/example/servingwebcontent/MovieListController.java
@@ -0,0 +1,32 @@
+
+package com.example.servingwebcontent;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MovieListController {
+
+	@GetMapping("/greeting")
+	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
+	}
+
+	@GetMapping("/navbar")
+	public String navbar() {
+		return "navbar";
+	}
+
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+
+	@GetMapping("/register")
+	public String register() {
+		return "register";
+	}
+}
