86890abafaf9f9aa2125bf243f6cf83e066fa815
Garrick <ContactGarrick@gmail.com>
2021-01-01 20:59:56 -0800

    no message

--- gs-serving-web-content/initial/src/main/java/com/example/servingwebcontent/MovieListController.java
+++ gs-serving-web-content/initial/src/main/java/com/example/servingwebcontent/MovieListController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class MovieListController {
 
-	@GetMapping("/")
+	@GetMapping("/navbar")
 	public String navbar() {
 		return "navbar";
 	}
